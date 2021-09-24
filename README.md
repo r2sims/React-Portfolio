66db7ebe010799b034059fa8d2320a4b6ca8749f
Rodrick Sims <rsimstransport@gmail.com>
2021-09-23 23:25:20 -0400

    updated files and folders

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# React-Portfolio
+# Unit 20 React Homework: React Portfolio
+
+Now that I've worked with React and have multiple projects to share, I'll be updating my portfolio and other materials to build toward being employer competitive. Creating a portfolio using React will help set me apart from other developers whose portfolios do not use some of the latest technologies.
+
+##Demo
+
+
+
+## Instructions
+
+* [Updated Portfolio](#updated-portfolio)
+
+* [Design](#design)
+
+* [React](#react)
+
+* [Updated LinkedIn Profile](#updated-linkedin-profile)
+
---
+++ src/resumeData.js
@@ -0,0 +1,63 @@
+let resumeData = {
+    "imagebaseurl":"https://r2sims.github.io/",
+    "name": "Rodrick Sims",
+    "role": "Full STack Web Developer",
+    "linkedinId":"63078216",
+
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://linkedin.com/in/rod-sims-63078216/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/r2sims",
+          "className":"fa fa-github"
+        },
+      
+      ],
+    "aboutme":"I am currently an Full Stack Web Developer",
+    "address":"Concord",
+    "website":"https://r2sims.github.io",
+    "education":[
+      {
+        "UniversityName":"UNCC Bootcamp",
+        "MonthOfPassing":"October",
+        "YearOfPassing":"2021"
+        
+      },
+      
+    ],
+    
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Express"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"NBA Player Search",
+        
+      },
+      {
+        "name":"project2",
+        "description":"Central Data Processor",
+       
+      },
+      
+    ]
+  }
+  
+  export default resumeData
